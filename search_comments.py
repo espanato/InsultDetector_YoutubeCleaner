18896c4248713012343522b2bc951cfedc301836
Tomas Espana <tomas.espana@student-cs.fr>
2021-11-14 17:17:13 +0100

    acces aux reponses

--- search_comments.py
+++ search_comments.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-# OBJ: renvoie sous forme de dico les commentaires d'une vidéo en particulier
-# le dico: les clés sont les pseudos des utilisateurs, les valeurs sont leurs commentaires
+# chercher les commentaires d'une video avec toutes les infos de yt
 
 
 def get_video_comments(video_id, nb=3):
@@ -16,6 +15,22 @@ def get_video_comments(video_id, nb=3):
     dico = json.loads(str(soup))
     return(dico)
 
+
+# chercher les réponse d'un commentaire avec toutes les infos de yt
+def get_video_replies(parent_id, nb=3):
+    requete = requests.get("https://youtube.googleapis.com/youtube/v3/comments?part=snippet&parentId=" +
+                           parent_id+"&textFormat=plainText&key=AIzaSyB13BBBdQR3muGiIR2dLoiycwZGQ30YYHs"+"&maxResults="+str(nb))
+    page = requete.content
+    soup = BeautifulSoup(page, features='html.parser')
+    dico = json.loads(str(soup))
+    return(dico)
+
+
+# print(get_video_replies('UgyXSKR2TSiAgzebL3R4AaABAg'))
+
+
+# OBJ: renvoie sous forme de dico les commentaires d'une vidéo
+# le dico: les clés sont les pseudos des utilisateurs, les valeurs sont les textes
 
 def get_video_comments_user_name(video_id, nb=3):
     commentaires_dico = {}
@@ -29,6 +44,10 @@ def get_video_comments_user_name(video_id, nb=3):
 
 # print(get_video_comments_info('vBFiBT2Z0EM', 3))
 
+
+# OBJ: idem qu'avant mais cette fois
+# le dico: les clés sont les id des commentaires, les valeurs sont les textes
+
 def get_video_comments_msg_id(video_id, nb=3):
     commentaires_dico = {}
     dico_comments = get_video_comments(video_id, nb)
@@ -39,11 +58,10 @@ def get_video_comments_msg_id(video_id, nb=3):
     return commentaires_dico
 
 
-print(get_video_comments_msg_id('vBFiBT2Z0EM'))
+# print(get_video_comments_msg_id('vBFiBT2Z0EM'))
 
-# OBJ: filtrer les commentaires qui possèdent une liste de mots
-# pour une vidéo, on filtre les commentaires qui incluent les mots dans la liste en argument
 
+# OBJ: filtrer les commentaires qui possèdent les mots d'une liste de mots
 
 def get_video_comments_words(video_id, liste_de_mots, nb=3):
     commentaires_dico = {}
@@ -78,6 +96,53 @@ def get_video_comments_words(video_id, liste_de_mots, nb=3):
 #    'vBFiBT2Z0EM', ["joue a", "joue", "qu'il", "qu"]))
 
 
-# OBJ:
+# OBJ: renvoie sous forme de dico les réponses d'un commentaire
+# le dico: les clés sont les pseudos des utilisateurs, les valeurs sont les textes
 
-# def_get_comments_author(video_id, nom_utilisateur, nb=3):
+def get_video_replies_dico(parent_id, nb=3):
+    reponses_dico = {}
+    dico_comments = get_video_replies(parent_id, nb)
+    for item in dico_comments['items']:
+        comment = item['snippet']['textDisplay']
+        id = item['id']
+        reponses_dico[id] = comment
+    return reponses_dico
+
+
+# print(get_video_replies_dico('UgyXSKR2TSiAgzebL3R4AaABAg'))
+
+
+# OBJ: filtrer les réponses qui possèdent les mots d'une liste de mots
+
+def get_video_replies_words(parent_id, liste_de_mots, nb=3):
+    commentaires_dico = {}
+    requete = requests.get(
+        "https://youtube.googleapis.com/youtube/v3/comments?part=snippet&parentId=" +
+        parent_id+"&textFormat=plainText&key=AIzaSyB13BBBdQR3muGiIR2dLoiycwZGQ30YYHs"+"&maxResults="+str(nb))
+    page = requete.content
+    soup = BeautifulSoup(page, features='html.parser')
+    dico = json.loads(str(soup))
+
+    for mot in liste_de_mots:
+        commentaires_dico[mot] = set()
+
+    # la liste des commentaires pour chaque mot, les mots sont les clés de commentaires_dico
+    for item in dico['items']:
+        comment = item['snippet']['textDisplay']
+
+        for mot in liste_de_mots:
+            if mot in comment:
+                commentaires_dico[mot].add(comment)
+
+    # on rajoute une clé 'tout' dont la valeur est l'ensemble des tweets qui contienne tous les mots
+    if len(liste_de_mots) > 1:
+        commentaires_dico['#tout'] = commentaires_dico[liste_de_mots[0]]
+        for i in range(1, len(liste_de_mots)):
+            commentaires_dico['#tout'] = commentaires_dico['#tout'].intersection(
+                commentaires_dico[liste_de_mots[i]])
+
+    return commentaires_dico
+
+
+print(get_video_replies_words('UgyXSKR2TSiAgzebL3R4AaABAg',
+      ['bac+3', 'bac + 3', 'imbecile', 'imbécile']))
