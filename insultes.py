ed994a661b576f74d6ba229a21c997bf2875540b
Blondel Hector <hector.blondel@student-cs.fr>
2021-11-15 14:04:00 +0100

    amélioration de la distance

--- insultes.py
+++ insultes.py
@@ -1,23 +1,47 @@
-liste_insultes = ["connard","salope"]
+from liste_insulte import insultes
 
+"""
 def levenshtein(a, b):
     if not a: return len(b)
     if not b: return len(a)
     return min(levenshtein(a[1:], b[1:])+(a[0] != b[0]),
                levenshtein(a[1:], b)+1,
                levenshtein(a, b[1:])+1)
-    
-print(levenshtein("cat","chello"))
+"""
+
+def distance(s, t):
+    if s == t:
+        return 0
+    if len(s) == 0:
+        return len(t)
+    if len(t) == 0:
+        return len(s)
+
+    v0 = list(range(len(t) + 1))
+    v1 = [None] * (len(t) + 1)
+
+    for i in range(len(s)):
+        v1[0] = i + 1
+        for j in range(len(t)):
+            cost = 0 if s[i] == t[j] else 1
+            v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+        for j in range(len(v0)):
+            v0[j] = v1[j]
+
+    return v1[len(t)]
+
+print(distance("caaevaevlt","ceavnljabvljahello"))
 
 
-def est_insulte(mot):
-    for ins in liste_insultes :
-        if levenshtein(mot,ins) <= 1 :
+def est_insulte(mot,precision=1):
+    for ins in insultes :
+        if distance(mot,ins) <= 1 :
             return True
-        else :
-            return False
+    return False
 
 
+if __name__ == "main":
+    est_insulte("salope")
 
 
 
@@ -31,5 +55,5 @@ def ajoute_mot(mot,arbre):
 
 ajoute_mot("connard",arbre)
 """
-def est_insulte(mot):
+
         
