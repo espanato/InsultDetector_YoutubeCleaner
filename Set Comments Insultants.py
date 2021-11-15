e3b3e4a8b8c92ed769352339b38957d41c16deb4
Hugo Bui <hugo.bui@student-cs.fr>
2021-11-15 15:09:57 +0100

    r

--- Set Comments Insultants.py
+++ Set Comments Insultants.py
@@ -1,26 +1,28 @@
-import keyboard 
+import keyboard
 import codecs
 import time
 from search_comments import dico_en_3
 
-dic1,dic2,dic3 = dico_en_3('TRY2eQju5nc','5r-ZsKH3uNE','6janW0h3cZY') 
+dic1, dic2, dic3 = dico_en_3('TRY2eQju5nc', '5r-ZsKH3uNE', '6janW0h3cZY', 3)
 print('Prêt à commencer !')
-for k,v in dic1.items():
-    i=1
-    c=0
+for k, v in dic2.items():
+    i = 1
+    c = 0
     print(v)
     time.sleep(2)
-    while c==0:
+    while c == 0:
         if keyboard.is_pressed('i'):
-            file = codecs.open('./Commentaires insultants/'+'Commentaire '+str(k)+'.txt','w',encoding = 'utf-8')
+            file = codecs.open('./Commentaires insultants/' +
+                               'Commentaire '+str(k)+'.txt', 'w', encoding='utf-8')
             file.write(v)
-            c=1
+            c = 1
         elif keyboard.is_pressed('n'):
-            file = codecs.open('./Commentaires neutres/'+'Commentaire '+str(k)+'.txt','w',encoding = 'utf-8')
+            file = codecs.open('./Commentaires neutres/' +
+                               'Commentaire '+str(k)+'.txt', 'w', encoding='utf-8')
             file.write(v)
-            c=2
-    if c==1:
+            c = 2
+    if c == 1:
         print(f'Le commentaire {k} est insultant')
-    elif c==2:
+    elif c == 2:
         print(f"Le commentaire {k} n'est pas insultant")
-    i+=1
+    i += 1
