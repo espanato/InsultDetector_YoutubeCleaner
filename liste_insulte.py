399f3c1c048d4501e69e6164cf3e14cb865f08c5
Blondel Hector <hector.blondel@student-cs.fr>
2021-11-15 14:43:57 +0100

    ajout des stop words

--- liste_insulte.py
+++ liste_insulte.py
@@ -2,5 +2,9 @@ with open("insultes.txt", "r") as fichier_insultes:
     insultes = fichier_insultes.read()
     insultes = insultes.split("\n")
 
-print(insultes)
-print(type(insultes))
+with open("non_insultes.txt","r") as fichier_non_insultes:
+    non_insultes = fichier_non_insultes.read().split("\n")
+
+if __name__ == "main":
+    print(insultes)
+    print(type(insultes))
