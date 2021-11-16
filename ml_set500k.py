e46b6fa1ca188eb5d23af1d0519c66046c849baf
Charly Bohard <charly.bohard@student-cs.fr>
2021-11-16 23:55:18 +0100

    Tentative opti hyperpara

--- ml_set500k.py
+++ ml_set500k.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import re
-import time
 
-ti = time.time()
+
 data = pd.read_csv('C:/Users/Charly Bohard/Documents/PythonScripts/jigsaw-toxic-comment-classification-challenge/train.csv')
 data = data.drop(['id','toxic','identity_hate','severe_toxic','obscene','threat'],axis=1)
 X, y = data["comment_text"], data['insult']
@@ -45,24 +44,48 @@ for sen in range(0, len(X)):
 
     documents.append(document)
 
-vectorizer = CountVectorizer(
-    max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('french'))
-X = vectorizer.fit_transform(documents).toarray()
+# vectorizer = CountVectorizer(
+#     max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('french'))
+# X = vectorizer.fit_transform(documents).toarray()
 
-tfidfconverter = TfidfTransformer()
-X = tfidfconverter.fit_transform(X).toarray()
+# tfidfconverter = TfidfTransformer()
+# X = tfidfconverter.fit_transform(X).toarray()
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=0)
 
-classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
+# classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+# classifier.fit(X_train, y_train)
+# y_pred = classifier.predict(X_test)
 
 
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
-print(accuracy_score(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
+# print(accuracy_score(y_test, y_pred))
 
-tf = time.time
-print(tf-ti)
+def opti_hyperpara():
+    precision = 0
+    max_feat_opti = 0
+    for n in range(2000,5001,1000):
+        vectorizer = CountVectorizer(
+        max_features=n, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+        X = vectorizer.fit_transform(documents).toarray()
+
+        tfidfconverter = TfidfTransformer()
+        X = tfidfconverter.fit_transform(X).toarray()
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=0)
+
+        classifier = RandomForestClassifier(n_estimators=2000, random_state=0)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+        mat = confusion_matrix(y_test,y_pred)
+        recall = mat[1][1] / (mat[1][0]+mat[1][1])
+        if recall > precision:
+            max_feat_opti = n
+            precision = recall
+        print(n,' : ',recall)
+    print(f'Précision de {precision} pour n_estimators = {max_feat_opti}')
+
+opti_hyperpara()
