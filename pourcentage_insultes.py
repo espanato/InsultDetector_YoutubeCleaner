8aabdfe85c6ab6f699bb880a20a57826291c0a7e
Tomas Espana <tomas.espana@student-cs.fr>
2021-11-17 10:47:52 +0100

    modifs

--- pourcentage_insultes.py
+++ pourcentage_insultes.py
@@ -2,30 +2,18 @@ from search_comments import *
 from contient_insulte import *
 
 
-def get_all_list(video_id):
-    """OBJ: récupérer la liste de tous les commentaires
-    la liste : les valeurs sont les textes des commentaires"""
-    commentaires_liste = []
-    dico_comments = get_all(video_id)
-    for text in dico_comments.values():
-        commentaires_liste.append(text)
-    return commentaires_liste
-
-# print(get_all_list("vBFiBT2Z0EM"))
-
-
 def percent_insultes(video_id):
     """OBJ : récupérer le pourcentage de commentaires insultants
     et la liste des insultes correspondantes dans chaque message"""
-    l_comm = get_all_list(video_id)
+    dico_comments = get_all(video_id)
     l_insultes = []
     n = 0
-    for comm in l_comm:
-        nb = nb_insultes(comm)[0]
+    for comm in dico_comments:
+        nb = nb_insultes(dico_comments[comm])[0]
         if nb >= 1:
             n = n + 1
-            l_insultes.append(nb_insultes(comm)[1])
-    return n/len(l_comm) * 100, l_insultes
+            l_insultes.append(nb_insultes(dico_comments[comm])[1])
+    return n/len(dico_comments) * 100, l_insultes
 
 
-print(percent_insultes("vBFiBT2Z0EM"))
+# print(percent_insultes("DHiTuMboqVI"))
