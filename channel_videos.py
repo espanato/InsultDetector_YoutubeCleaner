951c5930a947a495c58a68f9364621401763dcd5
Roby Edouard <edouard.roby@student-cs.fr>
2021-11-17 14:30:44 +0100

    Ajout d'une fonction pour avoir le titre d'une vidéo

--- channel_videos.py
+++ channel_videos.py
@@ -45,6 +45,28 @@ def presentation_caracteristiques_chaine(channel_id, nb=3):
     return(D)
 
 
+def get_channel_videos(channel_id, nb=2):
+    """Renvoie un dico contenant les vidéos d'une chaîne"""
+    requete = requests.get(
+        "https://youtube.googleapis.com/youtube/v3/search?key="+KEY+"&part=snippet&channelId="+channel_id+"&textFormat=plainText&order=viewCount&type=video"+"&maxResults="+str(nb))
+    page = requete.content
+    soup = BeautifulSoup(page, features='html.parser')
+    dico = json.loads(str(soup))
+    return(dico)
+
+
+def get_video_title(video_id):
+    """Renvoie le titre d'une vidéo en fonction de son id"""
+    requete = requests.get(
+        "https://youtube.googleapis.com/youtube/v3/videos?key="+KEY+"&part=snippet&id="+video_id+"&textFormat=plainText")
+    page = requete.content
+    soup = BeautifulSoup(page, features='html.parser')
+    dico = json.loads(str(soup))
+    return(dico['items'][0]['snippet']['title'])
+
+
+# print(get_video_title("ejWAoQUV6SE"))
+
 # print(get_channel_videos("UCUo1RqYV8tGjV38sQ8S5p9A"))
 # print(get_video_viewCount("Q0pBzowOKU4"))
 # print(presentation_caracteristiques_chaine("UCXwDLMDV86ldKoFVc_g8P0g"))
