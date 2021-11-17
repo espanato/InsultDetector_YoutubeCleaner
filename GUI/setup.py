29407233b416098b1766ce87309e0bb0eb4ed638
Hugo Bui <hugo.bui@student-cs.fr>
2021-11-17 14:45:35 +0100

    setup.py

--- GUI/setup.py
+++ GUI/setup.py
@@ -1,7 +1,8 @@
 from cx_Freeze import setup, Executable
 base = None
 executables = [Executable("main.py", base=base)]
-packages = ["idna", "bs4", "requests", "pyperclip", "dash", "nltk", "sklearn"]
+packages = ["idna", "bs4", "requests", "pyperclip", "dash", "nltk", "sklearn", "click", "google-auth-oauthlib",
+            "google-auth-httplib2", "tkinter", "jsom", "os", "csv", "codecs", "pickle", "time", "carbonai"]
 options = {
     'build_exe': {
         'packages': packages,
